95267cce7896982f3ff4fae4659523fc640d6915
Gopi Madabhushi <gopik@users.noreply.github.com>
2016-04-30 14:22:25 +0530

    Flow sensor specs

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
-
+/**
+ * YF-21 flow sensor specifications
+ *
+ * 120 L/H - 16  Hz - 480 Pulses/L
+ * 240 L/H - 32.5Hz - 487 Pulses/L
+ * 360 L/H - 49.3Hz - 493 Pulses/L
+ * 480 L/H - 65.5Hz - 491.25 
+ * 600 L/H - 82  Hz - 492
+ * 720 L/H - 90.2Hz - 451
 #include "main.h" 
 
 // -------- Global Variables --------- //
